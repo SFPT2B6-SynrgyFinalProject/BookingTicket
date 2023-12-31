8923151b3441dffa59df0de51edb0bab9e97152e
Arya Rezza Anantya <anantya4995@gmail.com>
2023-12-31 19:55:20 +0700

    Add new project ✨

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.firebase.firebase-perf")
+}
+
+android {
+    namespace = "id.synrgy6team2.bookingticket"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "id.synrgy6team2.bookingticket"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    // data binding ini hanya untuk library dari ImagePicker/CapturePhoto :(
+    buildFeatures { dataBinding = true }
+}
+
+dependencies {
+
+    api(project(":presentation"))
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    ksp("com.google.dagger:hilt-compiler:2.48.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-perf-ktx")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/id/synrgy6team2/bookingticket/MyApplications.kt
@@ -0,0 +1,9 @@
+package id.synrgy6team2.bookingticket
+
+import android.app.Application
+import androidx.work.Configuration
+import androidx.work.DelegatingWorkerFactory
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplications : Application()
