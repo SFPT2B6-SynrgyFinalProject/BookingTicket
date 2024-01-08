ccbe3dde825d99bcb88a8158a6d054f11b9b20f2
Arya Rezza Anantya <anantya4995@gmail.com>
2024-01-08 22:48:12 +0700

    Update code Login and Forgot Password Layout 🚩

--- presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/LupaPasswordActivity.kt
+++ presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/LupaPasswordActivity.kt
@@ -2,10 +2,17 @@ package id.synrgy6team2.bookingticket.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import id.synrgy6team2.bookingticket.presentation.databinding.ActivityLupaPasswordBinding
 
+@AndroidEntryPoint
 class LupaPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLupaPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lupa_password)
+        binding = ActivityLupaPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/RegisterActivity.kt
+++ presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/RegisterActivity.kt
@@ -2,10 +2,17 @@ package id.synrgy6team2.bookingticket.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import id.synrgy6team2.bookingticket.presentation.databinding.ActivityRegisterBinding
 
+@AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
