bf35d833eb116638ae3e2d7d21cbf5d3be956dae
pradanaadn <pradanaadn21@gmail.com>
2024-01-08 17:15:09 +0800

    Completed lupa password layout, and add text to res

---
+++ presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/LupaPasswordActivity.kt
@@ -0,0 +1,11 @@
+package id.synrgy6team2.bookingticket.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LupaPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lupa_password)
+    }
+}
