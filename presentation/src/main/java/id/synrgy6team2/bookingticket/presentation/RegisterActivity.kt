3113592df5c68df080b09748326bf1103ce9b6e9
Ali Yasin Mubarok <aliyasinmubarok@gmail.com>
2024-01-09 14:31:34 +0700

    Update Register Layout

--- presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/RegisterActivity.kt
+++ presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/RegisterActivity.kt
@@ -1,9 +1,11 @@
 package id.synrgy6team2.bookingticket.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import id.synrgy6team2.bookingticket.presentation.databinding.ActivityRegisterBinding
+import java.util.Calendar
 
 @AndroidEntryPoint
 class RegisterActivity : AppCompatActivity() {
@@ -14,5 +16,31 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tieDateOfBirth.setOnClickListener {
+            datePicker()
+        }
+
+
     }
+
+    private fun datePicker() {
+        val c = Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, _, monthOfYear, dayOfMonth ->
+                val date = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                binding.tilDateOfBirth.editText?.setText(date)
+            },
+            year, month, day
+        )
+        datePickerDialog.show()
+    }
+
+
 }
