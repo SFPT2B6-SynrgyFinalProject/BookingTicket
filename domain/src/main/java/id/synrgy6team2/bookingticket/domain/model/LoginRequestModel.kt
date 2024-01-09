dff314fcb9ad2e7fdd59e39ff204b227edcafba9
Arya Rezza Anantya <anantya4995@gmail.com>
2024-01-09 18:26:27 +0700

    Update data class model

--- domain/src/main/java/id/synrgy6team2/bookingticket/domain/model/LoginRequestModel.kt
+++ domain/src/main/java/id/synrgy6team2/bookingticket/domain/model/LoginRequestModel.kt
@@ -2,5 +2,6 @@ package id.synrgy6team2.bookingticket.domain.model
 
 data class LoginRequestModel(
     val email: String? = null,
-    val password: String? = null
+    val password: String? = null,
+    val googleToken: String? = null
 )
