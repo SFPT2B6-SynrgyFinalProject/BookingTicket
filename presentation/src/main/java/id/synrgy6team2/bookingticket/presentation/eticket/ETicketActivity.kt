4978efa62e64f4347eabd682ca9cf2ec391a6480
Gina Melinia <fourseasons.codes@gmail.com>
2024-02-06 14:35:03 +0700

    Add E-Ticket layout

---
+++ presentation/src/main/java/id/synrgy6team2/bookingticket/presentation/eticket/ETicketActivity.kt
@@ -0,0 +1,12 @@
+package id.synrgy6team2.bookingticket.presentation.eticket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.synrgy6team2.bookingticket.presentation.R
+
+class ETicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_e_ticket)
+    }
+}
